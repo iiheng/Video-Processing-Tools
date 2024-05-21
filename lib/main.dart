b50afebf36e5fd609c701dced32c874a9bec9f46
iiheng <1269305589@qq.com>
2024-05-21 18:06:53 +0800

    feat: 修改windows名称

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 import 'main_drawer.dart'; // 确保导入MainDrawer文件
+import 'package:window_manager/window_manager.dart';
+Future<void> main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  // Must add this line.
+  await windowManager.ensureInitialized();
 
-void main() {
+  WindowOptions windowOptions = const WindowOptions(
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    minimumSize: Size(400, 300)
+  );
+  windowManager.setTitle("视频处理软件");
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
   runApp(const MyApp());
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import ffmpeg_kit_flutter
+import screen_retriever
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
